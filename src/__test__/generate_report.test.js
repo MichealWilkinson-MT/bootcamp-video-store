886cc9ef17f6fd7f871c8b12a3dfc795bf99e144
Micheal Wilkinson <micheal.wilkinson@madetech.com>
2022-04-12 15:34:39 +0100

    New release 1 day test

--- src/__test__/generate_report.test.js
+++ src/__test__/generate_report.test.js
@@ -3,3 +3,7 @@ const { calculate_cost } = require('../generate_report');
 it('Should calculate a cost of £2 for a regular movie one day rental',  () => {
     expect(calculate_cost('REGULAR', 1)).toEqual(2.00)
 });
+
+it('Should calculate a cost of £3 for a new release movie one day rental', () => {
+    expect(calculate_cost('NEW_RELEASE', 1)).toEqual(3.00)
+});
